04ba07b0832a9942770a2fd4cbf1583623af991e
jayantk95 <jayantkulkarni999@gmail.com>
2020-06-29 12:42:46 +0530

    first commit

---
+++ build.sbt
@@ -0,0 +1,4 @@
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.5"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.12" % "2.4.5"
+
---
+++ src/main/scala/SimpleApp.scala
@@ -0,0 +1,16 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext				
+import org.apache.spark.SparkConf
+object SimpleApp {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("Spark pi")
+    val ssc = new SparkContext(conf)
+    val logFile = "/home/hduser/stackanalysis/data/stackexchange/stackoverflow.com-Posts/Posts100k.txt" 
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("scala")).count() 
+    val numBs = logData.filter(line => line.contains("java")).count()
+    println(s"Number of Logins: $numAs, Number of Logouts: $numBs")
+    ssc.stop()
+  }
+}
